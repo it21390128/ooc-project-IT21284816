d5f3b171a68a8dc4d6998b126c7c8c403fd0637b
it21390128 <99236910+it21390128@users.noreply.github.com>
2022-05-20 21:41:54 +0530

    Customer.h

---
+++ Customer.h
@@ -0,0 +1,58 @@
+//Kumarasinghe P.K.M.S
+//IT21390128
+class Customer
+{
+	protected:
+		char name[50];
+		char email[50];
+		char NIC[12];
+		char contactNo[10];
+		
+		Feedback *feedback[];
+		
+	public:
+		User();
+		User(const char uname[], const char uemail[], const char uNIC[],
+		const char ucontactNo[]);
+		void displayDetails();
+		void searchparks();
+		void addFeedback(Feedback*0);
+		~User();
+};
+
+Customer::Customer()
+{
+	    strcpy(name,"");
+	    strcpy(email,"");
+	    strcpy(NIC,"");
+	    strcpy (contactNo,"0000000000");
+}
+
+Customer::Customer(const char uname[], const char uemail[], const char uNIC[], 
+const char ucontactNo[])
+{
+	    strcpy(name, uname);
+	    strcpy(email, uemail);
+	    strcpy(NIC, uNIC);
+	    strcpy (contactNo, ucontactNo);
+}    
+
+void Customer::displayDetails()
+{
+	
+}
+
+void Customer::searchparks()
+{
+	
+}
+
+void Customer::addFeedback(Feedback*0)
+{
+	
+}
+
+Customer::~User()
+{
+	//Destructor
+}
